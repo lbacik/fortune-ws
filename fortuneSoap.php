57bcc380bfaeb06b60120b88f44d691e3643089f
lubac <mail@luka.sh>
2014-10-22 22:08:29 +0200

    rc 3

--- fortuneSoap.php
+++ fortuneSoap.php
@@ -6,19 +6,26 @@
  */
 class FortuneSrv
 {
+	
+	// Uwaga ponizsze komentarz w stylu javadoc jest umieszczny w opisie wsdl metody
+	
 	/*
-  	* Losujemy fortunkê z wszystkich plików w katalogu, b¹dŸ konkretnego pliku 
-	* 
-	* Funkcja wykorzystujê skrypt PHP Fortune
-	* - je¿eli parametr $path wskazuje na katalog to powinna zostaæ wykorzystana funkcja quoteFromDir
-	* - je¿eli parametr wskazuje plik, to funkcja: getRandomQuote
-	*
-	*	zwracany jest ci¹g ze znacznikami <br> - tak dzia³a wykorzystywana biblioteka
-	*
-	* @param string $path
-	* @return string
-	*/
-	function getFortuneStr($path = config\defaultPath)
+  	 * Losujemy fortunkê z wszystkich plików w katalogu, b¹dŸ konkretnego pliku 
+	 * 
+	 * Funkcja wykorzystujê skrypt PHP Fortune
+	 * - je¿eli parametr $path wskazuje na katalog to powinna zostaæ wykorzystana funkcja quoteFromDir
+	 * - je¿eli parametr wskazuje plik, to funkcja: getRandomQuote
+	 *
+	 *	W wersji Html zwracany jest ci¹g ze znacznikami <br> (tak dzia³a wykorzystywana biblioteka)
+	 *  funkcja getFortuneStr() zwraca tekst bez znaczników html
+	 */
+	
+	/**
+	 * 
+	 * @param string $path
+	 * @return string
+	 */
+	public function getFortuneHtml($path = config\defaultPath)
 	{
 		$fortune = '';
 		$f = new Fortune;
@@ -31,6 +38,16 @@ class FortuneSrv
 		else	$fortune = $f->quoteFromDir($p . "/");
 		
 		return $fortune;
+	}
+
+	/**
+	 * 
+	 * @param string $path
+	 * @return string
+	 */
+	public function getFortuneStr($path = config\defaultPath)
+	{
+		return strip_tags($this->getFortuneHtml($path));	
 	}
 	
 }
